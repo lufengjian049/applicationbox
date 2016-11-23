93b089b51aeeea1f07b2fff68f237437483fb073
lfj <lufengjian0409@163.com>
2016-11-23 16:10:55 +0800

    db host modify

--- config/config-default.js
+++ config/config-default.js
@@ -3,7 +3,7 @@ var config = {
     database: 'application',
     username: 'www',
     password: 'www',
-    host: '16r03151r6.iask.in',
+    host: 'princekin.vicp.io', //  16r03151r6.iask.in
     // host:'localhost',
     port: 3306
 };
