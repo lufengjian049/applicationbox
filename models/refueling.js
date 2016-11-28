bad93265eaea735f1630697c4ae343a301989576
lfj <lufengjian0409@163.com>
2016-11-28 17:06:30 +0800

    readme 修改

--- models/refueling.js
+++ models/refueling.js
@@ -43,3 +43,14 @@ module.exports = db.defineModel('refueling', {
 //         }
 //     }
 // }
+
+// 时间
+// 地点 position
+// 经度 longitude
+// 纬度 latitude
+// 型号(93#,95#) type --foreign key
+// 品类(石油，中石化，海油) category --foreign key
+// 单价 price
+// 本次费用 cost 
+// 本次油量 fuel
+// 里程？？ mileage
